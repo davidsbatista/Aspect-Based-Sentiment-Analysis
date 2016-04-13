2adad16bcfaf459cf07bfdb0cd0fa8edcf1e90a4
David S. Batista <dsbatista@gmail.com>
2016-04-13 23:52:36 +0200

    load 2014 corpus, and load StanfordCoreNLP interface

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import xml.etree.ElementTree as ET
 from libraries.baselines import Corpus
-import xml.etree.ElementTree as ET, getopt, logging, sys, random, re, copy
+
+from stanford_corenlp_python import jsonrpc
+from simplejson import loads
 
 
-def main():
-
-    # the train set is composed by train and trial dataset
+def process_semeval_2015():
+    # the train set is composed by train and trial data set
     corpora = dict()
-    corpora['restaurants'] = dict()
     corpora['laptop'] = dict()
     train_filename = 'datasets/ABSA-SemEval2015/ABSA-15_Restaurants_Train_Final.xml'
     trial_filename = 'datasets/ABSA-SemEval2015/absa-2015_restaurants_trial.xml'
@@ -21,17 +22,45 @@ def main():
     for r in reviews:
         sentences += r.find('sentences').getchildren()
 
+    # TODO: parser is not loading aspect words and opinioss
     corpus = Corpus(sentences)
+    corpus.size()
 
-    # TODO: testar com os dados de 2014, sacar daqui https://github.com/pedrobalage/SemevalAspectBasedSentimentAnalysis/tree/master/semeval_data
-    # TODO: alterar o parser para funcionar com os dados de 2015
-    print corpus.size
-    print corpus.aspect_terms_fd
-    print corpus.top_aspect_terms
-    print corpus.corpus[0].text
-    print corpus.corpus[0].id
-    print corpus.corpus[0].aspect_terms
-    print corpus.corpus[0].aspect_categories
+
+def process_semeval_2014():
+    # the train set is composed by train and trial dataset
+    corpora = dict()
+    corpora['restaurants'] = dict()
+    train_filename = 'datasets/ABSA-SemEval2014/Restaurants_Train_v2.xml'
+    trial_filename = 'datasets/ABSA-SemEval2014/restaurants-trial.xml'
+    corpus = Corpus(ET.parse(train_filename).getroot().findall('sentence') +
+                    ET.parse(trial_filename).getroot().findall('sentence'))
+    corpora['restaurants']['trainset'] = dict()
+    corpora['restaurants']['trainset']['corpus'] = corpus
+    return corpora
+
+
+def main():
+    # TODO: start corenlp server "python corenlp.py"
+
+    # interface for Stanford-Core-NLP server
+    server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(),
+                                 jsonrpc.TransportTcpIp(addr=("127.0.0.1",
+                                                              8080)))
+
+    result = loads(server.parse("Hello world.  It is so beautiful"))
+    print "Result", result
+
+    corpora = process_semeval_2014()
+    train_restaurants = corpora['restaurants']['trainset']['corpus']
+
+    for s in train_restaurants.corpus:
+        print s.text
+
+    """
+    print train_restaurants.size
+    print train_restaurants.aspect_terms_fd
+    """
 
 if __name__ == '__main__':
     main()
